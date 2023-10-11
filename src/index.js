8931e5c219a5812498d5b2a726956c5d691c4679
Kharis <khariss.ma14@gmail.com>
2023-10-11 15:34:09 +0700

    tes

--- src/index.js
+++ src/index.js
@@ -13,56 +13,40 @@ import GraphQL from './components/GraphQL';
 import Login from './components/Login';
 import OneGenre from './components/OneGenre';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {index: true, element: <Home />},
-      {
-        path: "/movies",
-        element: <Movies />
-      },
-      {
-        path: "/movies/:id",
-        element: <Movie />
-      },
-      {
-        path: "/genres",
-        element: <Genres />
-      },
-      {
-        path: "/genres/:id",
-        element: <OneGenre />
-      },
-      {
-        path: "/admin/movie/0",
-        element: <EditMovie />
-      },
-      {
-        path: "/admin/movie/:id",
-        element: <EditMovie />
-      },
-      {
-        path: "/manage-catalouge",
-        element: <ManageCatalouge />
-      },
-      {
-        path: "/graphQL",
-        element: <GraphQL />
-      },
-      {
-        path: "/login",
-        element: <Login />
-      },
-    ]
-  }
-])
+const router = createBrowserRouter({
+  basename: '/movies-fe', // Replace '/my-react-app' with your actual base URL
+  routes: [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: '/movies', element: <Movies /> },
+        { path: '/movies/:id', element: <Movie /> },
+        { path: '/genres', element: <Genres /> },
+        { path: '/genres/:id', element: <OneGenre /> },
+        { path: '/admin/movie/0', element: <EditMovie /> },
+        { path: '/admin/movie/:id', element: <EditMovie /> },
+        { path: '/manage-catalogue', element: <ManageCatalouge /> },
+        { path: '/graphql', element: <GraphQL /> },
+        { path: '/login', element: <Login /> },
+      ],
+    },
+    // Define your error route here (if needed)
+    {
+      path: '*', // This will match any route not matched by the previous routes
+      element: <ErrorPage />,
+    },
+  ],
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-); 
+try {
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+} catch (error) {
+  console.error('Error during rendering: ', error);
+}
